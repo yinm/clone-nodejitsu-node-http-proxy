48e8201c814c9b3bfafeb88708ec3ca1ba51cf67
yinm <yinm.cminor9@gmail.com>
2018-05-20 16:48:25 +0900

    Add should setup the correct headers

---
+++ test/lib-http-proxy-common-test.js
@@ -0,0 +1,47 @@
+const common = require('../lib/http-proxy/common')
+const url = require('url')
+const expect = require('expect.js')
+
+describe('lib/http-proxy/common.js', () => {
+  describe('#setupOutgoing', () => {
+    it ('should setup the correct headers', () => {
+      let outgoing = {}
+
+      common.setupOutgoing(outgoing,
+        {
+          agent: '?',
+          target: {
+            host: 'hey',
+            hostname: 'how',
+            socketPath: 'are',
+            port: 'you',
+          },
+          headers: {'fizz': 'bang', 'overwritten': true},
+          localAddress: 'local.address',
+          auth: 'username:pass',
+        },
+        {
+          method: 'i',
+          url: 'am',
+          headers: {'pro': 'xy', 'overwritten': false},
+        }
+      )
+
+      expect(outgoing.host).to.eql('hey')
+      expect(outgoing.hostname).to.eql('how')
+      expect(outgoing.socketPath).to.eql('are')
+      expect(outgoing.port).to.eql('you')
+      expect(outgoing.agent).to.eql('?')
+
+      expect(outgoing.method).to.eql('i')
+      expect(outgoing.path).to.eql('am')
+
+      expect(outgoing.headers.pro).to.eql('xy')
+      expect(outgoing.headers.fizz).to.eql('bang')
+      expect(outgoing.headers.overwritten).to.eql(true)
+      expect(outgoing.localAddress).to.eql('local.address')
+      expect(outgoing.auth).to.eql('username:pass')
+    })
+  })
+
+})
