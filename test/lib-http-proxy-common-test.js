ce198351e88769d98fdd2c47714236e106873971
yinm <yinm.cminor9@gmail.com>
2018-05-20 18:42:39 +0900

    Add should pass through https client parameters

--- test/lib-http-proxy-common-test.js
+++ test/lib-http-proxy-common-test.js
@@ -435,6 +435,41 @@ describe('lib/http-proxy/common.js', () => {
       })
     })
 
+    it('should pass through https client parameters', () => {
+      let outgoing = {}
+
+      common.setupOutgoing(
+        outgoing,
+        {
+          agent: '?',
+          target: {
+            host: 'how',
+            hostname: 'are',
+            socketPath: 'you',
+            protocol: 'https:',
+            pfx: 'my-pfx',
+            key: 'my-key',
+            passphrase: 'my-passphrase',
+            cert: 'my-cert',
+            ca: 'my-ca',
+            ciphers: 'my-ciphers',
+            secureProtocol: 'my-secure-protocol',
+          },
+        },
+        {
+          method: 'i',
+          url: 'am',
+        }
+      )
+
+      expect(outgoing.pfx).eql('my-pfx')
+      expect(outgoing.key).eql('my-key')
+      expect(outgoing.passphrase).eql('my-passphrase')
+      expect(outgoing.cert).eql('my-cert')
+      expect(outgoing.ca).eql('my-ca')
+      expect(outgoing.ciphers).eql('my-ciphers')
+      expect(outgoing.secureProtocol).eql('my-secure-protocol')
+    })
 
   })
 
