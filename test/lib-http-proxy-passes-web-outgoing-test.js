0aff8cd130912241c738c330240e07660011ae92
yinm <yinm.cminor9@gmail.com>
2018-05-22 10:48:33 +0900

    Add not when the redirected location does not match target host

--- test/lib-http-proxy-passes-web-outgoing-test.js
+++ test/lib-http-proxy-passes-web-outgoing-test.js
@@ -51,6 +51,13 @@ describe('lib/http-proxy/passes/web-outgoing.js', () => {
         expect(this.proxyRes.headers.location).to.eql('http://ext-manual.com/')
       })
 
+      it('not when the redirected location does not match target host', function() {
+        this.proxyRes.statusCode = 302
+        this.proxyRes.headers.location = 'http://some-other/'
+        httpProxy.setRedirectHostRewrite(this.req, {}, this.proxyRes, this.options)
+        expect(this.proxyRes.headers.location).to.eql('http://some-other/')
+      })
+
     })
 
   })
