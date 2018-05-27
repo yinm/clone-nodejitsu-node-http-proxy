7e0277c79b174276a411ea2dc6d6cc84d8f0b6f4
yinm <yinm.cminor9@gmail.com>
2018-05-27 14:56:16 +0900

    Add set the correct x-forwarded-* headers

--- test/ib-http-proxy-passes-web-incoming-test.js
+++ test/ib-http-proxy-passes-web-incoming-test.js
@@ -56,4 +56,23 @@ describe('lib/http-proxy/passes/web.js', () => {
     })
   })
 
+  describe('#XHeaders', () => {
+    let stubRequest = {
+      connection: {
+        remoteAddress: '192.168.1.2',
+        remotePort: '8080',
+      },
+      headers: {
+        host: '192.168.1.2:8080'
+      },
+    }
+
+    it('set the correct x-forwarded-* headers', () => {
+      webPasses.XHeaders(stubRequest, {}, { xfwd: true })
+      expect(stubRequest.headers['x-forwarded-for']).to.be('192.168.1.2')
+      expect(stubRequest.headers['x-forwarded-port']).to.be('8080')
+      expect(stubRequest.headers['x-forwarded-proto']).to.be('http')
+    })
+  })
+
 })
